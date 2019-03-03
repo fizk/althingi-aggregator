dcc2502c4c8227e14762a8fdb01980df4527bda8
Einar Adalsteinsson <einar.adalsteinsson@rea-group.com>
2019-03-03 17:17:58 +1100

    Adds to ./auto/assembly.sh

--- auto/assembly.sh
+++ auto/assembly.sh
@@ -5,5 +5,6 @@ SCRIPT=$( cd "$( dirname "$0" )" && pwd )/../public/index.php
 php ${SCRIPT} load:congressman --assembly=$1
 php ${SCRIPT} load:plenary --assembly=$1
 php ${SCRIPT} load:issue --assembly=$1
+php ${SCRIPT} load:tmp-speech --assembly=$1
 php ${SCRIPT} load:committee-assembly --assembly=$1
 php ${SCRIPT} load:plenary-agenda --assembly=$1
