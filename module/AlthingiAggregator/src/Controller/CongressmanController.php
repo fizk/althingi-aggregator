6fea7d9cb00ecbf21e101d862d6f5bceca713c12
Einar Valur Aðalsteinsson <fizk78@gmail.com>
2019-11-11 19:39:26 +1100

    Adds for ministries (#13)
    * Adds for ministries
    
    * Makes it pretty

--- module/AlthingiAggregator/src/Controller/CongressmanController.php
+++ module/AlthingiAggregator/src/Controller/CongressmanController.php
@@ -2,6 +2,7 @@
 namespace AlthingiAggregator\Controller;
 
 use AlthingiAggregator\Extractor\CommitteeSitting;
+use AlthingiAggregator\Extractor\MinisterSitting;
 use Zend\Mvc\Controller\AbstractActionController;
 use AlthingiAggregator\Lib\Consumer\ConsumerAwareInterface;
 use AlthingiAggregator\Lib\Provider\ProviderAwareInterface;
@@ -59,6 +60,58 @@ class CongressmanController extends AbstractActionController implements Consumer
                     "thingmenn/{$congressmanId}/nefndaseta",
                     '//þingmaður/nefndasetur/nefndaseta',
                     new CommitteeSitting()
+                );
+            }
+        }
+    }
+
+    /**
+     * Get Ministers.
+     * If additional parameter is passed (--assembly=int) than only congressman
+     * for given assembly will be fetched.
+     *
+     * If no additional params is given, only the congressmen are fetched but not
+     * their session in parliament.
+     *
+     * @throws \Exception
+     */
+    public function findMinisterAction()
+    {
+        $assemblyNumber = $this->params('assembly', null);
+        $congressmenUrl = ($assemblyNumber)
+            ? "https://www.althingi.is/altext/xml/radherrar/?lthing={$assemblyNumber}"
+            : "https://www.althingi.is/altext/xml/radherrar";
+        $congressmenElements = $this->queryForNoteList($congressmenUrl, '//ráðherralisti/ráðherra');
+
+        $this->saveDomNodeList(
+            $congressmenElements,
+            'thingmenn',
+            new Congressman()
+        );
+
+        if ($assemblyNumber) {
+            /** @var  $congressmanElement \DOMElement*/
+            foreach ($congressmenElements as $congressmanElement) {
+                $congressmanId = $congressmanElement->getAttribute('id');
+                $congressmanSessionUrl = trim(
+                    $congressmanElement->getElementsByTagName('þingseta')->item(0)->nodeValue //http://www.althingi.is/altext/xml/thingmenn/thingmadur/thingseta/?nr=1261
+                );
+                $congressmanCommitteeUrl = trim(
+                    $congressmanElement->getElementsByTagName('ráðherraseta')->item(0)->nodeValue //https://www.althingi.is/altext/xml/radherrar/radherraseta/?nr=1261
+                );
+
+                $this->queryAndSave(
+                    $congressmanSessionUrl,
+                    "thingmenn/{$congressmanId}/thingseta",
+                    '//þingmaður/þingsetur/þingseta',
+                    new Session()
+                );
+
+                $this->queryAndSave(
+                    $congressmanCommitteeUrl,
+                    "thingmenn/{$congressmanId}/radherraseta",
+                    '//einstaklingur/ráðherrasetur/ráðherraseta',
+                    new MinisterSitting()
                 );
             }
         }
